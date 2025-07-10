3045a0c4ecf7f420ec1d4a1fa7c1000f9f5dc491
EliteProTech <proelite455@gmail.com>
2025-07-10 15:59:25 +0100

    EliteProTech

---
+++ app.js
@@ -0,0 +1,98 @@
+{
+  "name": "ElitePro WhatsApp Bot",
+  "description": "WhatsApp bot powered by Baileys and built by EliteProTech",
+  "env": {
+    "YT_NAME": {
+      "description": "YouTube channel name",
+      "value": "YT: @EliteProTech"
+    },
+    "SOCIAL_M": {
+      "description": "Social media info",
+      "value": "GitHub: EliteProTech"
+    },
+    "LOCATION": {
+      "description": "Your location",
+      "value": "Nigeria, Port Harcourt"
+    },
+
+    "OWNER_NUMBER": {
+      "description": "Bot owner's WhatsApp number",
+      "value": "2348109263390"
+    },
+    "OWNER_NAME": {
+      "description": "Bot owner's name",
+      "value": "ElitePro"
+    },
+    "BOT_NAME": {
+      "description": "Bot name",
+      "value": "ELITE-PRO-V1"
+    },
+    "SESSION_ID": {
+      "description": "Your session ID",
+      "required": true
+    },
+
+    "PREFIX": {
+      "description": "Bot prefix",
+      "value": "."
+    },
+    "AUTO_RECORDING": {
+      "value": "false"
+    },
+    "AUTO_TYPING": {
+      "value": "false"
+    },
+    "AUTO_RECORD_TYPE": {
+      "value": "false"
+    },
+    "AUTO_READ": {
+      "value": "false"
+    },
+    "AUTO_BIO": {
+      "value": "true"
+    },
+    "ANTI_92": {
+      "value": "false"
+    },
+    "AUTO_SW_VIEW": {
+      "value": "true"
+    },
+    "WELCOME": {
+      "value": "true"
+    },
+    "AUTO_REACT": {
+      "value": "false"
+    },
+    "AUTO_LIKE_STATUS": {
+      "value": "true"
+    },
+
+    "ELITE_PRO_PP": {
+      "description": "Bot profile picture URL",
+      "value": "https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg"
+    },
+    "THEME_EMOJI": {
+      "value": "👨‍💻"
+    },
+
+    "PACKNAME": {
+      "value": "Sticker By"
+    },
+    "AUTHOR": {
+      "value": "ELITEPRO\\n\\nContact: +2348109263390"
+    },
+
+    "TYPE_MENU": {
+      "value": "v2"
+    },
+    "WM": {
+      "value": "Youtube @EliteProTech"
+    },
+    "LINK": {
+      "value": "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
+    }
+  },
+  "scripts": {
+    "postdeploy": "echo ElitePro WhatsApp Bot has been deployed!"
+  }
+}
